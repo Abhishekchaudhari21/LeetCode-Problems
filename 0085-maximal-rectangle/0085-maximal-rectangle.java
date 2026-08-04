33b7c7db4333ec3e5baf460a93a1d7ae247a2c4e
Abhishek Chaudhari <143903535+Abhishekchaudhari21@users.noreply.github.com>
2026-08-04 23:48:02 +0530

    Time: 14 ms (87.23%), Space: 51.7 MB (14.87%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -2,56 +2,43 @@ class Solution {
 
     public int maximalRectangle(char[][] matrix) {
 
-        if (matrix == null || matrix.length == 0)
-            return 0;
+        if (matrix.length == 0) return 0;
 
         int rows = matrix.length;
         int cols = matrix[0].length;
 
-        int[] heights = new int[cols];
+        int[] h = new int[cols + 1];     // Sentinel 0
+        int ans = 0;
 
-        int maxArea = 0;
+        ArrayDeque<Integer> st = new ArrayDeque<>();
 
         for (int i = 0; i < rows; i++) {
 
-            for (int j = 0; j < cols; j++) {
+            st.clear();
 
-                if (matrix[i][j] == '1')
-                    heights[j]++;
-                else
-                    heights[j] = 0;
+            for (int j = 0; j <= cols; j++) {
+
+                if (j < cols) {
+                    h[j] = matrix[i][j] == '1'
+                            ? h[j] + 1
+                            : 0;
+                }
+
+                while (!st.isEmpty() && h[st.peek()] > h[j]) {
+
+                    int height = h[st.pop()];
+
+                    int width = st.isEmpty()
+                            ? j
+                            : j - st.peek() - 1;
+
+                    ans = Math.max(ans, height * width);
+                }
+
+                st.push(j);
             }
-
-            maxArea = Math.max(maxArea, largestRectangle(heights));
         }
 
-        return maxArea;
-    }
-
-    private int largestRectangle(int[] heights) {
-
-        Stack<Integer> stack = new Stack<>();
-
-        int max = 0;
-
-        for (int i = 0; i <= heights.length; i++) {
-
-            int h = (i == heights.length) ? 0 : heights[i];
-
-            while (!stack.isEmpty() && h < heights[stack.peek()]) {
-
-                int height = heights[stack.pop()];
-
-                int width = stack.isEmpty()
-                        ? i
-                        : i - stack.peek() - 1;
-
-                max = Math.max(max, height * width);
-            }
-
-            stack.push(i);
-        }
-
-        return max;
+        return ans;
     }
 }
