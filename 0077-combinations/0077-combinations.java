880684d937fdb9689f63fe8cd1c7d7844fd857c2
Abhishek Chaudhari <143903535+Abhishekchaudhari21@users.noreply.github.com>
2026-08-08 21:56:04 +0530

    Time: 14 ms (96.68%), Space: 98 MB (66.13%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+
+        backtrack(1, n, k);
+
+        return ans;
+    }
+
+    private void backtrack(int start, int n, int k) {
+
+        if (path.size() == k) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        // Number of elements still required
+        int need = k - path.size();
+
+        // Leave exactly 'need' numbers available
+        for (int i = start; i <= n - need + 1; i++) {
+
+            path.add(i);
+
+            backtrack(i + 1, n, k);
+
+            path.remove(path.size() - 1);
+        }
+    }
+}
